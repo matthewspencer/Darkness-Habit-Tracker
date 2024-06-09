1931f1371b8388b98a59729267c5cdb2fe6ea637
Max <matthewspencer95@gmail.com>
2024-06-09 20:58:49 +0300

    fixed rounded shape of the XpStatusBar and HpStatus

--- app/src/main/java/com/android/darknesshabittracker/features_character/presentation/components/XpStatusBar.kt
+++ app/src/main/java/com/android/darknesshabittracker/features_character/presentation/components/XpStatusBar.kt
@@ -1,5 +1,7 @@
 package com.android.darknesshabittracker.features_character.presentation.components
 
+import android.content.res.Configuration
+import android.health.connect.datatypes.Device
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,16 +10,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
+import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.android.darknesshabittracker.R
 import com.android.darknesshabittracker.features_character.domain.Charachter
 import com.android.darknesshabittracker.ui.theme.PurpleGrey40
@@ -27,7 +33,7 @@ val dummyCharacter2 = Charachter(
     34,
     50,
     3,
-    56,
+    62,
     100
 )
 
@@ -36,11 +42,23 @@ fun XpStatusBar(
     modifier: Modifier,
     charachter: Charachter
 ) {
-
-    val barShape = RoundedCornerShape(
-        topStart = 8.dp,
-        bottomStart = 8.dp
+    val innerBarShape = RoundedCornerShape(
+        topStart = 12.dp,
+        topEnd = 12.dp,
+        bottomStart = 12.dp,
+        bottomEnd = 12.dp
     )
+
+    val topBarShape = RoundedCornerShape(
+        topStart = 12.dp,
+        topEnd = 0.dp,
+        bottomStart = 12.dp,
+        bottomEnd = 0.dp
+    )
+
+
+
+
 
     Box(modifier = modifier) {
         Column {
@@ -48,13 +66,18 @@ fun XpStatusBar(
             Box(modifier = modifier
                 .padding(start = 40.dp)
                 .fillMaxWidth()
+                .clip(innerBarShape)
                 .height(32.dp)
-                .background(PurpleGrey40)) {
+                .background(PurpleGrey40)
+                ) {
 
                 Box(modifier = modifier
                     .fillMaxWidth(charachter.currXp / charachter.lvlUpXp.toFloat())
                     .height(32.dp)
-                    .background(Color.Red)) {
+                    .clip(topBarShape)
+                    .background(Color.Red)
+
+                    ) {
 
                 }
             }
@@ -75,8 +98,8 @@ fun XpStatusBar(
     }
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
 fun XpStatusBarPreview() {
-    XpStatusBar(modifier = Modifier, dummyCharacter)
+    XpStatusBar(modifier = Modifier, dummyCharacter2)
 }
